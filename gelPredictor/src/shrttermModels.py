3068b1394bd1d85d8342c0872b64f05ceb7a2fdb
dmitryv56 <dmitryv56@walla.com>
2022-10-09 19:50:32 +0300

    fix invalid import

--- gelPredictor/src/shrttermModels.py
+++ gelPredictor/src/shrttermModels.py
@@ -17,7 +17,7 @@ from keras.layers import MaxPooling1D
 from keras.models import Sequential
 
 from predictor.predictor import Predictor
-from predictor.utility import msg2log, exec_time
+from sys_util.utils import exec_time
 
 logger=logging.getLogger(__name__)
 
